c5b53cea944b96b3eb18925c078024dc39c29473
Benjamin Ernst <benjamin.ernst@cloudogu.com>
2025-05-05 11:54:16 +0200

    #11 add import from global config

---
+++ migration/config/global.go
@@ -0,0 +1,84 @@
+package configuration
+
+import (
+	"context"
+	"fmt"
+	regConfig "github.com/cloudogu/k8s-registry-lib/config"
+	"log/slog"
+	"path"
+)
+
+var globalConfigKeysToKeep = []string{
+	"/certificate/*",
+	"/k8s/*",
+	"/proxy/*",
+}
+
+func (ci *ConfigImporter) importGlobalConfig(ctx context.Context, config globalConfig) error {
+	slog.Info("Importing global config...")
+
+	previousGlobalConfig, err := ci.globalConfigRepo.Get(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get global config: %w", err)
+	}
+
+	configToKeep := make(map[regConfig.Key]regConfig.Value)
+	for key, value := range previousGlobalConfig.GetAll() {
+		if shouldKeepGlobalConfigKey(key.String(), globalConfigKeysToKeep) {
+			configToKeep[key] = value
+		}
+	}
+
+	err = ci.globalConfigRepo.Delete(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to delete global config: %w", err)
+	}
+
+	gc, err := ci.globalConfigRepo.Create(ctx, regConfig.CreateGlobalConfig(map[regConfig.Key]regConfig.Value{}))
+	if err != nil {
+		return fmt.Errorf("failed to create global config: %w", err)
+	}
+
+	for kkey, kval := range configToKeep {
+		slog.Debug("Setting previous global config-entry", "key", kkey, "value", kval)
+		newGlobalConfig, err := gc.Set(kkey, kval)
+		if err != nil {
+			return fmt.Errorf("failed to set previous config key %s: %w", kkey, err)
+		}
+
+		gc = regConfig.GlobalConfig{Config: newGlobalConfig}
+	}
+
+	// import config from exporter
+	for _, kv := range config {
+		if shouldKeepGlobalConfigKey(kv.Key, globalConfigKeysToKeep) {
+			slog.Debug("Ignoring global config-key", "key", kv.Key)
+			continue
+		}
+
+		slog.Debug("Setting global config", "key", kv.Key, "value", kv.Value)
+		newGlobalConfig, err := gc.Set(regConfig.Key(kv.Key), regConfig.Value(kv.Value))
+		if err != nil {
+			return fmt.Errorf("failed to set config key %s: %w", kv.Key, err)
+		}
+
+		gc = regConfig.GlobalConfig{Config: newGlobalConfig}
+	}
+
+	_, err = ci.globalConfigRepo.SaveOrMerge(ctx, gc)
+	if err != nil {
+		return fmt.Errorf("failed to save new global config: %w", err)
+	}
+
+	return nil
+}
+
+func shouldKeepGlobalConfigKey(key string, keysToKeep []string) bool {
+	for _, pattern := range keysToKeep {
+		matched, err := path.Match(pattern, key)
+		if err == nil && matched {
+			return true
+		}
+	}
+	return false
+}
