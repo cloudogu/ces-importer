d8fc27d69415d07d40f9d9e7eb6c1ca21ae05fa1
Benjamin Ernst <benjamin.ernst@cloudogu.com>
2025-05-09 08:59:25 +0200

    #11 integrate configSyncer in job executer

--- cmd/import-job/executor.go
+++ cmd/import-job/executor.go
@@ -5,8 +5,15 @@ import (
 	"fmt"
 	"github.com/cloudogu/ces-importer/api/exporter"
 	"github.com/cloudogu/ces-importer/configuration"
+	migrationConfig "github.com/cloudogu/ces-importer/migration/config"
+
+	backupEcosystem "github.com/cloudogu/k8s-backup-operator/pkg/api/ecosystem"
+
 	"github.com/cloudogu/ces-importer/sync"
+	"github.com/cloudogu/k8s-registry-lib/repository"
+	"k8s.io/client-go/kubernetes"
 	"net/http"
+	ctrl "sigs.k8s.io/controller-runtime"
 )
 
 type dataSyncer interface {
@@ -28,11 +35,37 @@ func NewImportExecutor() (*ImportExecutor, error) {
 		return nil, fmt.Errorf("failed to read job configuration: %w", err)
 	}
 
-	_ = exporter.NewClient(jobConfig.API.ExporterApiKey, http.DefaultClient)
+	clusterConfig, err := ctrl.GetConfig()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get k8s cluster config: %w", err)
+	}
+
+	k8sClient, err := kubernetes.NewForConfig(clusterConfig)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create k8s client: %w", err)
+	}
+
+	configMaps := k8sClient.CoreV1().ConfigMaps(jobConfig.Namespace)
+	secrets := k8sClient.CoreV1().Secrets(jobConfig.Namespace)
+
+	exporterApiClient := exporter.NewClient(jobConfig.API.ExporterApiKey, http.DefaultClient)
+	globalConfigRepo := repository.NewGlobalConfigRepository(configMaps)
+	doguConfigRepo := repository.NewDoguConfigRepository(configMaps)
+	sensitiveDoguConfigRepo := repository.NewSensitiveDoguConfigRepository(secrets)
+
+	backupClient, err := backupEcosystem.NewForConfig(clusterConfig)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create backup schedule client: %w", err)
+	}
+	backupScheduleClient := backupClient.BackupSchedules(jobConfig.Namespace)
 
 	_ = sync.NewRsyncSyncer(jobConfig.API.ExporterHost, jobConfig.SSH.User, jobConfig.SSH.PrivateSSHKeyPath)
 
-	return &ImportExecutor{}, nil
+	cs := migrationConfig.NewConfigImporter(jobConfig.ExporterHost, exporterApiClient, globalConfigRepo, doguConfigRepo, sensitiveDoguConfigRepo, backupScheduleClient)
+
+	return &ImportExecutor{
+		configSyncer: cs,
+	}, nil
 }
 
 func (j ImportExecutor) Start(ctx context.Context) error {
