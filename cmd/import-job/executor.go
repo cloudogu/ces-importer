b1819e23c772ddbc5ab2ca0d7d189ef97e2d0ddf
Nico Franzeck <nico.franzeck@cloudogu.com>
2025-05-26 09:37:29 +0200

    [#13] Revert fqdn change from executer of job

--- cmd/import-job/executor.go
+++ cmd/import-job/executor.go
@@ -6,10 +6,7 @@ import (
 	"github.com/cloudogu/ces-importer/api/exporter"
 	"github.com/cloudogu/ces-importer/configuration"
 	"github.com/cloudogu/ces-importer/logging"
-	"github.com/cloudogu/ces-importer/migration"
 	migrationConfig "github.com/cloudogu/ces-importer/migration/config"
-	"log/slog"
-
 	backupEcosystem "github.com/cloudogu/k8s-backup-operator/pkg/api/ecosystem"
 
 	"github.com/cloudogu/ces-importer/sync"
@@ -24,15 +21,11 @@ type dataSyncer interface {
 
 type configSyncer interface {
 	SyncConfig(ctx context.Context) error
-	SyncCertificates(ctx context.Context) error
-	ChangeFQDN(ctx context.Context) error
-	Backup(ctx context.Context, backupType migrationConfig.BackupType) error
 }
 
 type ImportExecutor struct {
 	configSyncer
 	dataSyncer
-	coordinator configuration.Coordinator
 }
 
 func NewImportExecutor() (*ImportExecutor, error) {
@@ -77,14 +70,8 @@ func NewImportExecutor() (*ImportExecutor, error) {
 
 	cs := migrationConfig.NewConfigImporter(jobConfig.DoguVolumeBasePath, exporterService.ConfigService, globalConfigRepo, doguConfigRepo, sensitiveDoguConfigRepo, backupScheduleClient)
 
-	co, err := configuration.ReadCoordinatorConfig()
-	if err != nil {
-		return nil, fmt.Errorf("failed to get coordinator configuration: %w", err)
-	}
-
 	return &ImportExecutor{
 		configSyncer: cs,
-		coordinator:  co,
 	}, nil
 }
 
@@ -108,45 +95,6 @@ func (j ImportExecutor) Start(ctx context.Context) (e error) {
 	//if err != nil {
 	//	return fmt.Errorf("failed to sync data: %w", err)
 	//}
-
-	if j.coordinator.Migration.ChangeFQDN && migration.IsFinalMigration(ctx) {
-		slog.Info("Change FQDN in final Migration")
-		err = j.configSyncer.Backup(ctx, migrationConfig.Backup)
-		// The defer function is only started, if we are in a final migration
-		// therefor it do not check for final migration again
-		defer func() {
-			slog.Info("Run Cleanup after final Migration")
-			if e != nil {
-				slog.Info("There is an error during fqdn or certificate sync -> Restore old values and cleanup backup")
-				err = j.configSyncer.Backup(ctx, migrationConfig.Restore)
-				if err != nil {
-					slog.Error("failed to restore backup of fqdn and certificates in final migration: %w because of %w", err, e)
-					e = fmt.Errorf("failed to restore backup of fqdn and certificates in final migration: %w because of %w", err, e)
-				}
-			} else {
-				slog.Info("There is no error during fqdn or certificate sync -> just cleanup backup")
-				err = j.configSyncer.Backup(ctx, migrationConfig.Cleanup)
-				if err != nil {
-					slog.Error("failed to cleanup backup of fqdn and certificates in final migration: %w because of %w", err, e)
-					e = fmt.Errorf("failed to cleanup backup of fqdn and certificates in final migration: %w because of %w", err, e)
-				}
-			}
-		}()
-		if err != nil {
-			slog.Error("failed to backup fqdn and certificates in final migration: %w", err)
-			return fmt.Errorf("failed to backup fqdn and certificates in final migration: %w", err)
-		}
-		err = j.configSyncer.SyncCertificates(ctx)
-		if err != nil {
-			slog.Error("failed to sync certificates in final migration: %w", err)
-			return fmt.Errorf("failed to sync certificates in final migration: %w", err)
-		}
-		err = j.configSyncer.ChangeFQDN(ctx)
-		if err != nil {
-			slog.Error("failed to change fqdn in final migration: %w", err)
-			return fmt.Errorf("failed to change fqdn in final migration: %w", err)
-		}
-	}
 
 	return nil
 }
