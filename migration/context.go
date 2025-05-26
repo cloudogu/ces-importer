bf6d3c022ae7b7bb14cbee45e872d1d50a88d650
Nico Franzeck <nico.franzeck@cloudogu.com>
2025-05-26 11:30:59 +0200

    [#13] Add method to set and get trigger fqdn change from context

--- migration/context.go
+++ migration/context.go
@@ -1,10 +1,19 @@
 package migration
 
-import "context"
+import (
+	"context"
+	"log/slog"
+	"os"
+)
 
 type migrationContextKeyType uint8
 
-const finalMigrationKey migrationContextKeyType = iota
+const (
+	finalMigrationKey migrationContextKeyType = iota
+	triggerFQDNChange
+)
+
+const envTriggerFQDNChange = "TRIGGER_FQDN_CHANGE"
 
 func SetFinalMigration(ctx context.Context) context.Context {
 	return context.WithValue(ctx, finalMigrationKey, true)
@@ -12,6 +21,28 @@ func SetFinalMigration(ctx context.Context) context.Context {
 
 func IsFinalMigration(ctx context.Context) bool {
 	isSet, ok := ctx.Value(finalMigrationKey).(bool)
+	if !ok {
+		return false
+	}
+
+	return isSet
+}
+
+func SetTriggerFQDNChange(ctx context.Context) context.Context {
+	return context.WithValue(ctx, triggerFQDNChange, true)
+}
+
+func SetTriggerFQDNChangeFromEnv(ctx context.Context) context.Context {
+	if os.Getenv(envTriggerFQDNChange) != "" {
+		ctx = SetTriggerFQDNChange(ctx)
+		slog.Debug("Trigger for fqdn change has been set.")
+	}
+
+	return ctx
+}
+
+func TriggerFQDNChange(ctx context.Context) bool {
+	isSet, ok := ctx.Value(triggerFQDNChange).(bool)
 	if !ok {
 		return false
 	}
--- migration/context_test.go
+++ migration/context_test.go
@@ -29,3 +29,57 @@ func TestIsFinalMigration(t *testing.T) {
 		assert.False(t, IsFinalMigration(context.Background()))
 	})
 }
+
+func TestSetTriggerFQDNChange(t *testing.T) {
+	ctx := SetTriggerFQDNChange(context.Background())
+
+	result, ok := ctx.Value(triggerFQDNChange).(bool)
+	assert.True(t, ok)
+	assert.True(t, result)
+}
+
+func TestSetTriggerFQDNChangeFromEnv(t *testing.T) {
+	tests := []struct {
+		name     string
+		envValue string
+		expected bool
+	}{
+		{
+			name:     "env to trigger fqdn change is set",
+			envValue: "true",
+			expected: true,
+		},
+		{
+			name:     "env to trigger fqdn change is empty",
+			envValue: "",
+			expected: false,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Setenv(envTriggerFQDNChange, tc.envValue)
+
+			ctx := SetTriggerFQDNChangeFromEnv(context.Background())
+
+			_, ok := ctx.Value(triggerFQDNChange).(bool)
+			assert.Equal(t, tc.expected, ok)
+		})
+	}
+}
+
+func TestTriggerFQDNChange(t *testing.T) {
+	t.Run("Trigger fqdn change should be true", func(t *testing.T) {
+		ctx := context.WithValue(context.Background(), triggerFQDNChange, true)
+		assert.True(t, TriggerFQDNChange(ctx))
+	})
+
+	t.Run("Trigger fqdn change should be false", func(t *testing.T) {
+		ctx := context.WithValue(context.Background(), triggerFQDNChange, false)
+		assert.False(t, TriggerFQDNChange(ctx))
+	})
+
+	t.Run("Trigger fqdn change should be false when not set", func(t *testing.T) {
+		assert.False(t, TriggerFQDNChange(context.Background()))
+	})
+}
